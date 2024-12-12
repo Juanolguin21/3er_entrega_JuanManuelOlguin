f27c85b5a3b0c9180f5f17755fe0ebefe6015902
Juan manuel <waltercito100@hotmail.com>
2024-12-12 16:37:02 -0300

    inicio entrega3 v7

--- Proyecto/Neoprenos/admin.py
+++ Proyecto/Neoprenos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from Neoprenos.models import neoprenos,pedido,cliente,sucursal
 # Register your models here.
+
+
+admin.site.register(neoprenos)
+
+admin.site.register(pedido)
+
+admin.site.register(sucursal)
+
+admin.site.register(cliente)
--- Proyecto/Neoprenos/models.py
+++ Proyecto/Neoprenos/models.py
@@ -5,6 +5,9 @@ class neoprenos(models.Model) :
     marca =models.CharField(max_length=30)
     modelo=models.CharField(max_length=30)
     serie=models.IntegerField()
+    tipo=models.CharField(max_length=20)
+    def __str__(self):
+        return f"Marca: {self.marca}  | Modelo:{self.modelo} | Medida:{self.serie} | Tipo:{self.tipo}"
     
 class cliente(models.Model) :
     nombre=models.CharField(max_length=30)
@@ -12,6 +15,8 @@ class cliente(models.Model) :
     email=models.EmailField()
     direccion=models.CharField(max_length=30)
     telefono=models.CharField(max_length=30)
+    def __str__(self):
+        return f"Nombre: {self.nombre}  | Apellido:{self.apellido} | Email:{self.email} | Direccion:{self.direccion} | Telefono:{self.telefono}"
     
 class sucursal(models.Model):
     nombre=models.CharField(max_length=30)
@@ -19,8 +24,12 @@ class sucursal(models.Model):
     telefono=models.CharField(max_length=30)
     direccion=models.CharField(max_length=30)
     email=models.EmailField()
+    def __str__(self):
+        return f"Nombre: {self.nombre}  | Pais:{self.pais} | telefono:{self.telefono} | direccion:{self.direccion} | email:{self.email}"
     
 class pedido(models.Model):
     cantidad=models.IntegerField()
     fechapedido=models.DateField()
     entregado=models.BooleanField()
+    def __str__(self):
+        return f"Cantidad: {self.cantidad}  | Fecha pedido:{self.fechapedido} | Entregado:{self.entregado}"

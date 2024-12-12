fe38c66eaf9a198d7163771c34927d2860c8c3e7
Juan manuel <waltercito100@hotmail.com>
2024-12-12 15:32:32 -0300

    inicio entrega3 v6

--- Proyecto/Neoprenos/urls.py
+++ Proyecto/Neoprenos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from Neoprenos import views
 
 urlpatterns = [
-      path('inicio/', views.inicio),
-      path('trajes/', views.trajes),
-      path('login/', views.login),
-      path('tienda/', views.tienda),
-      path('contacto/', views.contacto),
-      path('sucursales/', views.sucursales)
+      path('inicio/', views.inicio, name='Inicio'),
+      path('trajes/', views.trajes,name='Trajes'),
+      path('login/', views.login,name='Login'),
+      path('tienda/', views.tienda,name='Tienda'),
+      path('contacto/', views.contacto,name='Contacto'),
+      path('sucursales/', views.sucursales,name='Sucursales')
 ]

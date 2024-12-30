b801a07866dd3651d2abb990d2b8d344412f8501
Juan manuel <waltercito100@hotmail.com>
2024-12-30 17:18:21 -0300

    inicio entrega3 v28

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
--- Proyecto/users/urls.py
+++ Proyecto/users/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', views.login_request, name="Login" ),
     path('register/', views.register, name="Register" ),
-    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='Logout')
+    path('logout/', LogoutView.as_view(template_name='Neoprenos/inicio.html'), name='Logout')
 
 ]
